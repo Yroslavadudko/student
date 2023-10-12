eea891a3e1ae0318fee2173781dbded43d4174fa
Yroslavadudko <dudkomykola@icloud.com>
2023-10-12 20:31:59 +0300

    hw_14. final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Student leader = new Student(1, "Mykola", "Dudko");
+        Student leader = new Student( 1, "Mykola", "Dudko");
         Group group = new Group(leader);
 
         Student student1 = new Student(2, "Nataly", "Dudka");
         Student student2 = new Student(3, "Yaroslava", "Dudko");
+        Student student3 = new Student(4, "Valery", "Dudko");
+        Student student4 = new Student(5, "Harucka", "Dudko");
+        Student student5 = new Student(6, "Looloo", "Dudko");
+        Student student6 = new Student(7, "Sergiy", "Sherbina");
 
         group.addStudent(student1);
         group.addStudent(student2);
+        group.addStudent(student3);
+        group.addStudent(student4);
+        group.addStudent(student5);
+        group.addStudent(student6);
 
-        group.addTask("Homework 1");
+        group.addTask("Homework 14"); //adding a task
         group.addTask("Project Presentation");
+
+        group.changeLeader(student2); //change the leader
+
+        group.markTaskAsCompLeader(student4, "Homework 14"); //mark the task as completed
+        group.markTaskAsCompLeader(student5, "Project Presentation");
+
+        group.removeStudent(student6); // delete a student
+
+        group.renameStudent(student1, "Nataliy", "Dudko-Sherbina"); //Rename the student
 
         group.displayGroupInfo();
     }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,6 +1,7 @@
 package org.example;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Student {
@@ -39,4 +40,66 @@ public class Student {
         String logMessage = String.format("Student ID: %d, Name: %s %s", studentId, firstName, lastName);
         logger.info(logMessage);
     }
+
 }
+class Group {
+    private Student leader;
+    private List<Student> studentList;
+    private List<String> tasks;
+    private static final Logger logger = Logger.getLogger(Group.class.getName());
+
+    public Group(Student leader) {
+        this.leader = leader;
+        this.studentList = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        this.studentList.add(leader);
+    }
+    public void changeLeader(Student newLeader){
+        if (studentList.contains(newLeader)){
+            leader = newLeader;
+        }else {
+            logger.warning("New leader must be a member of the group.");
+        }
+    }
+    public void addStudent(Student student){
+        studentList.add(student);
+    }
+    public void removeStudent(Student student){
+        if (student != leader){
+            studentList.remove(student);
+        }else {
+            logger.warning("The leader cannot be removed from the group.");
+        }
+    }
+
+    public void renameStudent(Student student, String newFirstName, String newLastName){
+        if (student != leader){
+            student.setFirstName(newFirstName);
+            student.setLastName(newLastName);
+        }else {
+            logger.warning("The leader's name cannot be changed this way.");
+        }
+    }
+
+    public void addTask(String task){
+        tasks.add(task);
+    }
+    public void markTaskAsCompLeader(Student student, String task) {
+        if (studentList.contains(student)){
+            logger.info("Student " + student.getFirstName() + " marked task '" + task + "' as comleted.");
+        }else {
+            logger.warning("Student must be a member of the group to mark a task as completed.");
+        }
+    }
+    public void displayGroupInfo(){
+        logger.info("Group Leader: " + leader.getFirstName() + " " + leader.getLastName());
+        for (Student student : studentList) {
+            student.logStudentInfo();
+        }
+        for (String task : tasks){
+            logger.info("Task: " + task);
+        }
+
+    }
+}
+
